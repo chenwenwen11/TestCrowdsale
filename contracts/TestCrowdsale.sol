547c768a24c9545030e0f2d84d8de3bbdfbce301
Wenwen Chen <>
2018-04-15 20:54:32 +0800

    added more functions in crowdsale contract

--- contracts/TestCrowdsale.sol
+++ contracts/TestCrowdsale.sol
@@ -4,10 +4,42 @@ import "zeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/validation/WhitelistedCrowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
 import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import '../contracts/TestToken.sol';
+
 
 
 contract TestCrowdsale is CappedCrowdsale, RefundableCrowdsale, WhitelistedCrowdsale {
 
+
+	// ICO Stage
+  // ============
+  enum CrowdsaleStage { PreICO, ICO }
+  CrowdsaleStage public stage = CrowdsaleStage.PreICO; // By default it's Pre Sale
+  // =============
+
+  // Token Distribution
+  // =============================
+  uint256 public maxTokens = 100000000000000000000; // There will be total 100 Hashnode Tokens
+  uint256 public tokensForEcosystem = 20000000000000000000;
+  uint256 public tokensForTeam = 10000000000000000000;
+  uint256 public tokensForBounty = 10000000000000000000;
+  uint256 public totalTokensForSale = 60000000000000000000; // 60 HTs will be sold in Crowdsale
+  uint256 public totalTokensForSaleDuringPreICO = 20000000000000000000; // 20 out of 60 HTs will be sold during PreICO
+  // ==============================
+
+  // Amount raised in PreICO
+  // ==================
+  uint256 public totalWeiRaisedDuringPreICO;
+  // ===================
+
+
+  // Events
+  event EthTransferred(string text);
+  event EthRefunded(string text);
+
+
+ // Constructor
+  // ============
   function TestCrowdsale(uint256 _openingTime, uint256 _closingTime, uint256 _rate, address _wallet, uint256 _cap, MintableToken _token, uint256 _goal) public
     Crowdsale(_rate, _wallet, _token)
     CappedCrowdsale(_cap)
@@ -16,6 +48,93 @@ contract TestCrowdsale is CappedCrowdsale, RefundableCrowdsale, WhitelistedCrowd
     WhitelistedCrowdsale()
   {
     require(_goal <= _cap);
+  }
+  // ==================
+
+  // Crowdsale Stage Management
+  // =========================================================
+
+  // Change Crowdsale Stage. Available Options: PreICO, ICO
+  function setCrowdsaleStage(uint value) public onlyOwner {
+
+      CrowdsaleStage _stage;
+
+      if (uint(CrowdsaleStage.PreICO) == value) {
+        _stage = CrowdsaleStage.PreICO;
+      } else if (uint(CrowdsaleStage.ICO) == value) {
+        _stage = CrowdsaleStage.ICO;
+      }
+
+      stage = _stage;
+
+      if (stage == CrowdsaleStage.PreICO) {
+        setCurrentRate(5);
+      } else if (stage == CrowdsaleStage.ICO) {
+        setCurrentRate(2);
+      }
+  }
+
+  // Change the current rate
+  function setCurrentRate(uint256 _rate) private {
+      rate = _rate;
+  }
+
+  // ================ Stage Management Over =====================
+
+  // Token Purchase
+  // =========================
+  function () external payable {
+      uint256 tokensThatWillBeMintedAfterPurchase = msg.value.mul(rate);
+      if ((stage == CrowdsaleStage.PreICO) && (token.totalSupply() + tokensThatWillBeMintedAfterPurchase > totalTokensForSaleDuringPreICO)) {
+        msg.sender.transfer(msg.value); // Refund them
+        EthRefunded("PreICO Limit Hit");
+        return;
+      }
+
+      buyTokens(msg.sender);
+
+      if (stage == CrowdsaleStage.PreICO) {
+          totalWeiRaisedDuringPreICO = totalWeiRaisedDuringPreICO.add(msg.value);
+      }
+  }
+
+  function forwardFunds() internal {
+      if (stage == CrowdsaleStage.PreICO) {
+          wallet.transfer(msg.value);
+          EthTransferred("forwarding funds to wallet");
+      } else if (stage == CrowdsaleStage.ICO) {
+          EthTransferred("forwarding funds to refundable vault");  
+          super._forwardFunds();
+    
+      }
+  }
+  // ===========================
+
+  // Finish: Mint Extra Tokens as needed before finalizing the Crowdsale.
+  // ====================================================================
+
+  function finish(address _teamFund, address _ecosystemFund, address _bountyFund) public onlyOwner {
+
+      require(!isFinalized);
+      uint256 alreadyMinted = token.totalSupply();
+      require(alreadyMinted < maxTokens);
+
+      uint256 unsoldTokens = totalTokensForSale - alreadyMinted;
+      if (unsoldTokens > 0) {
+        tokensForEcosystem = tokensForEcosystem + unsoldTokens;
+      }
+
+      token.transfer(_teamFund,tokensForTeam);
+      token.transfer(_ecosystemFund,tokensForEcosystem);
+      token.transfer(_bountyFund,tokensForBounty);
+      finalize();
+  }
+  // ===============================
+
+  // REMOVE THIS FUNCTION ONCE YOU ARE READY FOR PRODUCTION
+  // USEFUL FOR TESTING `finish()` FUNCTION
+  function hasEnded() public view returns (bool) {
+    return true;
   }
 
 }
--- migrations/2_TestCrowdsale.js
+++ migrations/2_TestCrowdsale.js
@@ -2,8 +2,8 @@ var TestCrowdsale = artifacts.require("./TestCrowdsale.sol");
 var TestToken = artifacts.require("./TestToken.sol");
 
 module.exports = function(deployer) {
-  	const startTime = web3.eth.getBlock('latest').timestamp + 120;
-  	const endTime = startTime + (86400 * 30); // 30 days;
+  	const startTime = Math.round((new Date(Date.now()).getTime())/1000);
+  	const endTime = startTime + (86400 * 2); // 30 days;
 
   	deployer.deploy(TestToken).then(function() {
   		return deployer.deploy(TestCrowdsale,	
@@ -11,9 +11,9 @@ module.exports = function(deployer) {
 		    endTime,
 		    5, 
 		    "0x5AEDA56215b167893e80B4fE645BA6d5Bab767DE", 
-		    2000000000000000000, // 2 ETH
+		    500000000000000000000 , // 500 ETH
 		    TestToken.address,
-		    500000000000000000000 // 500 ETH
+		    2000000000000000000 // 2 ETH
 		);
 	});
 };
